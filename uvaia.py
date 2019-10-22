fc4aeb665daf03aff369b6c655be136feab0b62f
Adriano Soares <adriano@asimovtrading.com>
2019-10-22 13:29:37 -0300

    Foi

--- uvaia.py
+++ uvaia.py
@@ -4,14 +4,10 @@ import os
 import time
 
 from src import InstaBot
-from src.check_status import check_status
-from src.feed_scanner import feed_scanner
-from src.follow_protocol import follow_protocol
-from src.unfollow_protocol import unfollow_protocol
 
 bot = InstaBot(
     login="uvaiaoficial",
-    password="95422106",
+    password="amandalinda",
     like_per_day=1500,
     comments_per_day=0,
     tag_list=['alimentacaosaudavel',
@@ -84,63 +80,8 @@ bot = InstaBot(
     unfollow_whitelist=[])
 
 while True:
-
-    # print("# MODE 0 = ORIGINAL MODE BY LEVPASHA")
-    # print("## MODE 1 = MODIFIED MODE BY KEMONG")
-    # print("### MODE 2 = ORIGINAL MODE + UNFOLLOW WHO DON'T FOLLOW BACK")
-    # print("#### MODE 3 = MODIFIED MODE : UNFOLLOW USERS WHO DON'T FOLLOW YOU BASED ON RECENT FEED")
-    # print("##### MODE 4 = MODIFIED MODE : FOLLOW USERS BASED ON RECENT FEED ONLY")
-    # print("###### MODE 5 = MODIFIED MODE : JUST UNFOLLOW EVERYBODY, EITHER YOUR FOLLOWER OR NOT")
-
-    ################################
-    ##  WARNING   ###
-    ################################
-
-    # DON'T USE MODE 5 FOR A LONG PERIOD. YOU RISK YOUR ACCOUNT FROM GETTING BANNED
-    # USE MODE 5 IN BURST MODE, USE IT TO UNFOLLOW PEOPLE AS MANY AS YOU WANT IN SHORT TIME PERIOD
-
+    
     mode = 0
-
-    # print("You choose mode : %i" %(mode))
-    # print("CTRL + C to cancel this operation or wait 30 seconds to start")
-    # time.sleep(30)
 
     if mode == 0:
         bot.new_auto_mod()
-
-    elif mode == 1:
-        check_status(bot)
-        while bot.self_following - bot.self_follower > 200:
-            unfollow_protocol(bot)
-            time.sleep(10 * 60)
-            check_status(bot)
-        while bot.self_following - bot.self_follower < 400:
-            while len(bot.user_info_list) < 50:
-                feed_scanner(bot)
-                time.sleep(5 * 60)
-                follow_protocol(bot)
-                time.sleep(10 * 60)
-                check_status(bot)
-
-    elif mode == 2:
-        bot.bot_mode = 1
-        bot.new_auto_mod()
-
-    elif mode == 3:
-        bot.bot_mode = 3
-        while bot.login_status == 1:
-            bot.unfollow_recent_feed()
-            time.sleep(5)
-
-    elif mode == 4:
-        feed_scanner(bot)
-        time.sleep(60)
-        follow_protocol(bot)
-        time.sleep(10 * 60)
-
-    elif mode == 5:
-        bot.bot_mode = 2
-        unfollow_protocol(bot)
-
-    else:
-        print("Wrong mode!")
